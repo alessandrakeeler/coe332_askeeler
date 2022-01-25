3c69063b017cb7589bfb66ebb55e195fd4f73e1c
askeeler <askeeler@isp02.tacc.utexas.edu>
2022-01-25 11:35:14 -0600

    Init

---
+++ homework_01/integers.py
@@ -0,0 +1,13 @@
+
+def even_or_odd(int_list):
+    for i in int_list:
+        if i%2 == 0:
+            print(str(i) + " is even")
+            print(" ")
+        else:
+            print(str(i) + " is odd")
+            print(" ")
+
+
+intlist = [1,2,3,4,5,6,7,8,9,10]
+even_or_odd(intlist)
